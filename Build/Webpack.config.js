f90de1cd6dff5462d4546c6f12143f74a15b1894
Sascha <sascha8a@gmail.com>
2018-01-03 16:02:45 +0100

    Automatic generation of the Meta.xml

--- Build/Webpack.config.js
+++ Build/Webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
   },
   plugins: [
     new HardSourceWebpackPlugin({
-      cacheDirectory: '.cache'
+      cacheDirectory: '../../../../.cache'
     }),
     new TSLintPlugin({
       files: files,
