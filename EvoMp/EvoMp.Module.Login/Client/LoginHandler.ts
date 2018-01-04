263c655a58970b1861a24d1c5236054c7de39825
Sascha <sascha8a@gmail.com>
2018-01-04 21:03:38 +0100

    Made yarn build smaller

--- EvoMp/EvoMp.Module.Login/Client/LoginHandler.ts
+++ EvoMp/EvoMp.Module.Login/Client/LoginHandler.ts
@@ -2,8 +2,10 @@
 
 import {Cef} from '../../EvoMp.Module.Cef/Client/Cef'
 
-API.onResourceStart.connect(() => {
+let resourceStartHandler = API.onResourceStart.connect(() => {
+  API.triggerServerEvent('ready')
   API.sendChatMessage('Initialised!')
   const cefWindow: Cef = new Cef('Login', 'dist/Login.html', {chat: false, hud: false, cursor: true})
   cefWindow.load()
+  resourceStartHandler.disconnect()
 })
