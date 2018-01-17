c17ff31cf183a1f407f8fab38f6454b6993089ab
Christian Groothoff <ruffo324@gmail.com>
2018-01-17 15:11:01 +0100

    VehicleHandler #12 - Untubifing file

--- EvoMp/EvoMp.Core.Core/Server/Main.cs
+++ EvoMp/EvoMp.Core.Core/Server/Main.cs
@@ -8,141 +8,141 @@ using GrandTheftMultiplayer.Server.API;
 
 namespace EvoMp.Core.Core.Server
 {
-	public class Main : Script
-	{
-		#region DebugKonstante
+    public class Main : Script
+    {
+        #region DebugKonstante
 
 #if DEBUG
-		public const bool Debug = true;
+        public const bool Debug = true;
 #else
         public const bool Debug = false;
 #endif
 
-		#endregion //DebugKonstante
+        #endregion //DebugKonstante
 
-		public Main()
-		{
-			try
-			{
-				#region Core preparing / initialization
+        public Main()
+        {
+            try
+            {
+                #region Core preparing / initialization
 
-				// Clear console, set console color & write copyright
-				ConsoleUtils.Clear();
+                // Clear console, set console color & write copyright
+                ConsoleUtils.Clear();
 
-				// Prepare Console set title
-				ConsoleHandler.Server.ConsoleHandler.PrepareConsole();
+                // Prepare Console set title
+                ConsoleHandler.Server.ConsoleHandler.PrepareConsole();
 
-				#endregion // Core preparing / initialization
+                #endregion // Core preparing / initialization
 
-				ConsoleUtils.SetConsoleTitle("EvoMp GT-MP Server Core. All rights reserverd.");
+                ConsoleUtils.SetConsoleTitle("EvoMp GT-MP Server Core. All rights reserverd.");
 
-				// Load Console params
-				ParameterHandler.LoadParams();
+                // Load Console params
+                ParameterHandler.LoadParams();
 
-				// Load logo
-				ParameterHandler.SetDefault("LogoPath", "./ServerFiles/Default_Logo.txt");
-				string asciiLogoFile = ParameterHandler.GetValue("LogoPath");
+                // Load logo
+                ParameterHandler.SetDefault("LogoPath", "./ServerFiles/Default_Logo.txt");
+                string asciiLogoFile = ParameterHandler.GetValue("LogoPath");
 
-				#region Logo, Copyright, Server informations
+                #region Logo, Copyright, Server informations
 
-				ConsoleOutput.PrintLine("-", "~#E6E6E6~");
+                ConsoleOutput.PrintLine("-", "~#E6E6E6~");
 
-				// Write logo from logo file
-				ConsoleOutput.WriteCentredText(ConsoleType.Note,
-					ConsoleUtils.ParseTextFileForConsole($"{asciiLogoFile}", 2, 1));
+                // Write logo from logo file
+                ConsoleOutput.WriteCentredText(ConsoleType.Note,
+                    ConsoleUtils.ParseTextFileForConsole($"{asciiLogoFile}", 2, 1));
 
-				// No Logo defined -> message and use default Logo
-				if (ParameterHandler.IsDefault("LogoPath"))
-					ConsoleOutput.WriteCentredText(ConsoleType.Config,
-						$"Using logo file ~o~\"{Path.GetFullPath($"{asciiLogoFile}")}\"~;~.\n" +
-						$"Please start your server with the ~b~" +
-						$"LogoPath ~;~ " +
-						$"parameter.");
+                // No Logo defined -> message and use default Logo
+                if (ParameterHandler.IsDefault("LogoPath"))
+                    ConsoleOutput.WriteCentredText(ConsoleType.Config,
+                        $"Using logo file ~o~\"{Path.GetFullPath($"{asciiLogoFile}")}\"~;~.\n" +
+                        $"Please start your server with the ~b~" +
+                        $"LogoPath ~;~ " +
+                        $"parameter.");
 
-				// GetServerGamemodes writes cfg message to if not setten
-				string moduleTypesString =
-					string.Join(", ",
-						ModuleTypeHandler.GetServerGamemodes().ToList().ConvertAll(input => input.ToUpper()));
+                // GetServerGamemodes writes cfg message to if not setten
+                string moduleTypesString =
+                    string.Join(", ",
+                        ModuleTypeHandler.GetServerGamemodes().ToList().ConvertAll(input => input.ToUpper()));
 
-				const string leftServerInfo = "~#90A4AE~";
-				const string rightServerInfo = "~#ECEFF1~";
+                const string leftServerInfo = "~#90A4AE~";
+                const string rightServerInfo = "~#ECEFF1~";
 
-				// Tiny gray line & Empty
-				ConsoleOutput.PrintLine(" ");
+                // Tiny gray line & Empty
+                ConsoleOutput.PrintLine(" ");
 
-				// Small centered line with headline & developer
-				ConsoleOutput.WriteCentredText(ConsoleType.Note,
-					"".PadRight(55, '-') + "\n" +
-					"Server information\n" +
-					"".PadRight(55, '-'));
+                // Small centered line with headline & developer
+                ConsoleOutput.WriteCentredText(ConsoleType.Note,
+                    "".PadRight(55, '-') + "\n" +
+                    "Server information\n" +
+                    "".PadRight(55, '-'));
 
-				ConsoleOutput.WriteCentredText(ConsoleType.Info,
-					$"{leftServerInfo}{"Server mode:".PadRight(20)}{string.Empty.PadRight(5)}{rightServerInfo}{$"{moduleTypesString}".PadRight(20)}\n" +
-					$"{leftServerInfo}{"Runtime mode:".PadRight(20)}{string.Empty.PadRight(5)}{rightServerInfo}{$"{(Debug ? "Debugging" : "Release")}".PadRight(20)}\n" +
-					$"{leftServerInfo}{"Server name:".PadRight(20)}{string.Empty.PadRight(5)}{rightServerInfo}{$"{API.getServerName().Substring(0, 20)}".PadRight(20)}\n" +
-					$"{leftServerInfo}{"Server port:".PadRight(20)}{string.Empty.PadRight(5)}{rightServerInfo}{$"{API.getServerPort():0000}".PadRight(20)}\n" +
-					$"{leftServerInfo}{"Max players:".PadRight(20)}{string.Empty.PadRight(5)}{rightServerInfo}{$"{API.getMaxPlayers():0000}".PadRight(20)}\n");
+                ConsoleOutput.WriteCentredText(ConsoleType.Info,
+                    $"{leftServerInfo}{"Server mode:".PadRight(20)}{string.Empty.PadRight(5)}{rightServerInfo}{$"{moduleTypesString}".PadRight(20)}\n" +
+                    $"{leftServerInfo}{"Runtime mode:".PadRight(20)}{string.Empty.PadRight(5)}{rightServerInfo}{$"{(Debug ? "Debugging" : "Release")}".PadRight(20)}\n" +
+                    $"{leftServerInfo}{"Server name:".PadRight(20)}{string.Empty.PadRight(5)}{rightServerInfo}{$"{API.getServerName().Substring(0, 20)}".PadRight(20)}\n" +
+                    $"{leftServerInfo}{"Server port:".PadRight(20)}{string.Empty.PadRight(5)}{rightServerInfo}{$"{API.getServerPort():0000}".PadRight(20)}\n" +
+                    $"{leftServerInfo}{"Max players:".PadRight(20)}{string.Empty.PadRight(5)}{rightServerInfo}{$"{API.getMaxPlayers():0000}".PadRight(20)}\n");
 
-				// One empty lines
-				ConsoleOutput.PrintLine(" ");
+                // One empty lines
+                ConsoleOutput.PrintLine(" ");
 
-				// Small centered line with headline & developer
-				ConsoleOutput.WriteCentredText(ConsoleType.Note,
-					"".PadRight(55, '-') + "\n" +
-					"Developer team\n" +
-					"".PadRight(55, '-'));
+                // Small centered line with headline & developer
+                ConsoleOutput.WriteCentredText(ConsoleType.Note,
+                    "".PadRight(55, '-') + "\n" +
+                    "Developer team\n" +
+                    "".PadRight(55, '-'));
 
-				const string usernameColor = "~#ECEFF1~";
-				const string diTitleColor = "~#03A9F4~";
-				const string depyTitleColor = "~#4FC3F7~";
-				const string staffTitleColor = "~#B3E5FC~";
+                const string usernameColor = "~#ECEFF1~";
+                const string diTitleColor = "~#03A9F4~";
+                const string depyTitleColor = "~#4FC3F7~";
+                const string staffTitleColor = "~#B3E5FC~";
 
-				ConsoleOutput.WriteCentredText(ConsoleType.Note,
-					$"{diTitleColor}{"Roleplay Director".PadRight(20)}{string.Empty.PadRight(5)}{usernameColor}{"DevGrab".PadRight(20)}\n" +
-					$"{diTitleColor}{"Freeroam Director".PadRight(20)}{string.Empty.PadRight(5)}{usernameColor}{"Ruffo/Christian".PadRight(20)}\n" +
-					$"{depyTitleColor}{"Roleplay Deputy".PadRight(20)}{string.Empty.PadRight(5)}{usernameColor}{"Sascha".PadRight(20)}\n" +
-					$"{staffTitleColor}{"Roleplay Staff".PadRight(20)}{string.Empty.PadRight(5)}{usernameColor}{"Koka".PadRight(20)}\n" +
-					$"{staffTitleColor}{"Roleplay Staff".PadRight(20)}{string.Empty.PadRight(5)}{usernameColor}{"Lukas/Nitac".PadRight(20)}\n" +
-					$"{staffTitleColor}{"Roleplay Staff".PadRight(20)}{string.Empty.PadRight(5)}{usernameColor}{"Sopex".PadRight(20)}\n" +
-					$"{staffTitleColor}{"Roleplay Staff".PadRight(20)}{string.Empty.PadRight(5)}{usernameColor}{"Gary".PadRight(20)}\n" +
-					$"{staffTitleColor}{"Freeroam Staff".PadRight(20)}{string.Empty.PadRight(5)}{usernameColor}{"James".PadRight(20)}\n");
+                ConsoleOutput.WriteCentredText(ConsoleType.Note,
+                    $"{diTitleColor}{"Roleplay Director".PadRight(20)}{string.Empty.PadRight(5)}{usernameColor}{"DevGrab".PadRight(20)}\n" +
+                    $"{diTitleColor}{"Freeroam Director".PadRight(20)}{string.Empty.PadRight(5)}{usernameColor}{"Ruffo/Christian".PadRight(20)}\n" +
+                    $"{depyTitleColor}{"Roleplay Deputy".PadRight(20)}{string.Empty.PadRight(5)}{usernameColor}{"Sascha".PadRight(20)}\n" +
+                    $"{staffTitleColor}{"Roleplay Staff".PadRight(20)}{string.Empty.PadRight(5)}{usernameColor}{"Koka".PadRight(20)}\n" +
+                    $"{staffTitleColor}{"Roleplay Staff".PadRight(20)}{string.Empty.PadRight(5)}{usernameColor}{"Lukas/Nitac".PadRight(20)}\n" +
+                    $"{staffTitleColor}{"Roleplay Staff".PadRight(20)}{string.Empty.PadRight(5)}{usernameColor}{"Sopex".PadRight(20)}\n" +
+                    $"{staffTitleColor}{"Roleplay Staff".PadRight(20)}{string.Empty.PadRight(5)}{usernameColor}{"Gary".PadRight(20)}\n" +
+                    $"{staffTitleColor}{"Freeroam Staff".PadRight(20)}{string.Empty.PadRight(5)}{usernameColor}{"James".PadRight(20)}\n");
 
-				ConsoleOutput.PrintLine(" ");
+                ConsoleOutput.PrintLine(" ");
 
-				ParameterHandler.PrintArgs();
+                ParameterHandler.PrintArgs();
 
-				ConsoleOutput.PrintLine(" ");
-				ConsoleOutput.PrintLine("-");
+                ConsoleOutput.PrintLine(" ");
+                ConsoleOutput.PrintLine("-");
 
-				#endregion Logo, Copyright, Server informations
+                #endregion Logo, Copyright, Server informations
 
-				// Write information about Core startup
-				ConsoleOutput.WriteLine(ConsoleType.Core, "Initializing EvoMp Core...");
+                // Write information about Core startup
+                ConsoleOutput.WriteLine(ConsoleType.Core, "Initializing EvoMp Core...");
 
-				// Init ModuleStructurer
-				ModuleStructurer moduleStructurer = new ModuleStructurer();
+                // Init ModuleStructurer
+                ModuleStructurer moduleStructurer = new ModuleStructurer();
 
-				// Copy modules & NuGet files to Server
-				moduleStructurer.RefreshResourceModules();
-				moduleStructurer.CopyNuGetPackagesToServer();
+                // Copy modules & NuGet files to Server
+                moduleStructurer.RefreshResourceModules();
+                moduleStructurer.CopyNuGetPackagesToServer();
 
-				// Write complete & loading modules message
-				ConsoleOutput.WriteLine(ConsoleType.Core, "Initializing EvoMp Core completed.");
+                // Write complete & loading modules message
+                ConsoleOutput.WriteLine(ConsoleType.Core, "Initializing EvoMp Core completed.");
 
-				Shared.OnOnModuleLoadingStart(API);
+                Shared.OnOnModuleLoadingStart(API);
 
-				// Load Modules
-				new ModuleLoader(API).Load();
+                // Load Modules
+                new ModuleLoader(API).Load();
 
-				// Finish sequence
-				Shared.OnOnCoreStartupCompleted();
-				ConsoleOutput.WriteLine(ConsoleType.Core, "Core startup completed");
-				ConsoleOutput.PrintLine("-");
-			}
-			catch (Exception e)
-			{
-				ConsoleOutput.FinalConsoleWrite(e.ToString(), true);
-			}
-		}
-	}
+                // Finish sequence
+                Shared.OnOnCoreStartupCompleted();
+                ConsoleOutput.WriteLine(ConsoleType.Core, "Core startup completed");
+                ConsoleOutput.PrintLine("-");
+            }
+            catch (Exception e)
+            {
+                ConsoleOutput.FinalConsoleWrite(e.ToString(), true);
+            }
+        }
+    }
 }
