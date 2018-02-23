b33bb160ae38b6e758b6fea25cd68b7d9949ea91
Sascha8a <sascha@localhost.systems>
2018-02-23 18:27:26 +0100

    yarn build now uses node-env production

--- Build/web.js
+++ Build/web.js
@@ -5,6 +5,7 @@ async function main () {
   let files = Glob.sync('EvoMp/*/Web/*.+(html|pug)')
 
   for (const file of files) {
+    process.env.NODE_ENV = 'production'
     await new Bundler(file, {outDir: 'GTMP_Server/resources/EvoMp/dist'}).bundle()
   }
 }
