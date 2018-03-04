b5d14c2f5e5cb7c3aecb62686a2783bb4a3e32ae
Christian Groothoff <ruffo324@gmail.com>
2018-03-04 01:09:44 +0100

    #39 Moving BotModule Context & Repository to Server folder.

--- EvoMp/EvoMp.Module.BotHandler/Server/BotContext.cs
+++ EvoMp/EvoMp.Module.BotHandler/Server/BotContext.cs
@@ -2,11 +2,20 @@ using System;
 using System.Data.Entity;
 using System.Data.Entity.Migrations;
 using EvoMp.Core.Module.Server;
+using EvoMp.Module.BotHandler.Server.Entity;
 
-namespace EvoMp.Module.BotHandler.Server.Entity
+namespace EvoMp.Module.BotHandler.Server
 {
+    /// <inheritdoc />
+    /// <summary>
+    /// Initialize the BotContext and contains the tables
+    /// </summary>
     public class BotContext : DbContext
     {
+        /// <inheritdoc />
+        /// <summary>
+        /// Constructor. Based on DbContext and loads the connection string
+        /// </summary>
         public BotContext() : base(Environment.GetEnvironmentVariable("NameOrConnectionString"))
         {
             // Database logging
@@ -23,6 +32,9 @@ namespace EvoMp.Module.BotHandler.Server.Entity
             modelBuilder.Conventions.Add(new NonPublicColumnAttributeConvention());
         }
 
+        /// <summary>
+        /// Initalize the Bot Context, sets the migrator config and open the database connection
+        /// </summary>
         public void FirstInit()
         {
             Database.SetInitializer<BotContext>(null);
@@ -39,8 +51,16 @@ namespace EvoMp.Module.BotHandler.Server.Entity
         }
 
         #region Tables
-
+        /// <summary>
+        /// Table: Bots
+        /// Contains all recorded bots
+        /// </summary>
         public DbSet<BotDto> Bots { get; set; }
+
+        /// <summary>
+        /// Table: BotWaypoints
+        /// Contains all recorded Waypoints for a bot
+        /// </summary>
         public DbSet<BotWaypointDto> BotWaypoints { get; set; }
 
         #endregion Tables
