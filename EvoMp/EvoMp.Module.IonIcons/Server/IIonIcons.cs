de4d5fe9b6a0bf3bfcd52cca7e0faa88d5089480
Christian Groothoff <ruffo324@gmail.com>
2018-03-10 04:05:52 +0100

    #36 - Adding IonIcons module. Adding Client-Side only template

---
+++ EvoMp/EvoMp.Module.IonIcons/Server/IIonIcons.cs
@@ -0,0 +1,9 @@
+using EvoMp.Core.Module.Server;
+
+namespace EvoMp.Module.Ionicons.Server
+{
+    [ModuleProperties("shared", "Sascha", "Module for Ionic Icons.")]
+    public interface IIonIcons
+    {
+    }
+}
---
+++ EvoMp/EvoMp.Module.IonIcons/Server/IonIcons.cs
@@ -0,0 +1,8 @@
+using EvoMp.Core.Module.Server;
+
+namespace EvoMp.Module.Ionicons.Server
+{
+    public class IonIcons : BaseModule, IIonIcons
+    {
+    }
+}
