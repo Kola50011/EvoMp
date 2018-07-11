5c4c00f37c33127564bbbb887f20e506f8229f9f
Christian Groothoff <ruffo324@gmail.com>
2018-07-12 00:14:13 +0200

    Updating Nuget Packages. Ignoring Newtonsoft cause of GTMP using old version.

--- EvoMp/EvoMp.Module.Login/Server/Authentication/Authentication.cs
+++ EvoMp/EvoMp.Module.Login/Server/Authentication/Authentication.cs
@@ -24,7 +24,7 @@ namespace EvoMp.Module.Login.Server.Authentication
             _eventHandler = eventHandler;
             _clientHandler = clientHandler;
 
-            //_eventHandler.SubscribeToServerEvent("ready", new ServerEventHandle(OnPlayerReadyHandler));
+            _eventHandler.SubscribeToServerEvent("ready", new ServerEventHandle(OnPlayerReadyHandler));
             _eventHandler.SubscribeToServerEvent("AuthRequest", new ServerEventHandle(AuthRequest));
         }
 
