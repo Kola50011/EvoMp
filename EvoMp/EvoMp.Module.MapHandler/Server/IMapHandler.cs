a5974856ad1ccd40a63e9d54b3fdb6f54d2123ba
tmerge <tmerge@uos.de>
2018-07-17 18:41:25 +0200

    #14 new try to check in files also

---
+++ EvoMp/EvoMp.Module.MapHandler/Server/IMapHandler.cs
@@ -0,0 +1,10 @@
+using EvoMp.Core.Module.Server;
+
+namespace EvoMp.Module.MapHandler.Server
+{
+    [ModuleProperties("shared", "James, Ruffo", "Module to handle maps.")]
+    public interface IMapHandler
+    {
+
+    }
+}
---
+++ EvoMp/EvoMp.Module.MapHandler/Server/MapHandler.cs
@@ -0,0 +1,14 @@
+using GrandTheftMultiplayer.Server.API;
+
+namespace EvoMp.Module.MapHandler.Server
+{
+    public class MapHandler
+    {
+        private readonly API api;
+
+        public MapHandler(API api)
+        {
+            this.api = api;
+        }
+    }
+}
