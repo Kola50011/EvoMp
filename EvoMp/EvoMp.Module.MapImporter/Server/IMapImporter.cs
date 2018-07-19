6c55bdc2bcfc4bc3890176c88726e0cb240e5a3d
Christian Groothoff <ruffo324@gmail.com>
2018-07-19 21:57:23 +0200

    #14 - Adding Project "MapImporter"

---
+++ EvoMp/EvoMp.Module.MapImporter/Server/IMapImporter.cs
@@ -0,0 +1,9 @@
+using EvoMp.Core.Module.Server;
+
+namespace EvoMp.Module.MapImporter.Server
+{
+    [ModuleProperties("shared", "Ruffo, James", "Module to import maps.")]
+    public interface IMapImporter
+    {
+    }
+}
---
+++ EvoMp/EvoMp.Module.MapImporter/Server/MapImporter.cs
@@ -0,0 +1,8 @@
+using EvoMp.Core.Module.Server;
+
+namespace EvoMp.Module.MapImporter.Server
+{
+    public class MapImporter : BaseModule, IMapImporter
+    {
+    }
+}
