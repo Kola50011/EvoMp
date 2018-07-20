1d6e13eb2f6970c646c86502bddbceeee5b1fae6
tmerge <tmerge@uos.de>
2018-07-20 11:27:04 +0200

    #12 refactor utils methods

--- EvoMp/EvoMp.Module.VehicleHandler/Server/Utils.cs
+++ EvoMp/EvoMp.Module.VehicleHandler/Server/Utils.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using EvoMp.Core.Module.Server;
 using GrandTheftMultiplayer.Server.API;
 using GrandTheftMultiplayer.Shared;
 using GrandTheftMultiplayer.Shared.Gta.Vehicle;
@@ -10,32 +9,21 @@ namespace EvoMp.Module.VehicleHandler.Server
 {
     public class Utils : IUtils
     {
+
         /// <inheritdoc />
         public List<VehicleHash> GetVehiclesByName(string searchVehicleName)
         {
-            List<VehicleHash> vehicleHashes = new List<VehicleHash>();
             searchVehicleName = searchVehicleName.ToLower();
-
-            // Name like
-            vehicleHashes.AddRange(Enum.GetValues(typeof(VehicleHash)).Cast<VehicleHash>()
-                .Where(vehicleHash =>
-                    searchVehicleName == $"{vehicleHash}".ToLower() && !vehicleHashes.Contains(vehicleHash)));
-
-            //Name starts with
-            vehicleHashes.AddRange(Enum.GetValues(typeof(VehicleHash)).Cast<VehicleHash>().Where(vehicleHash =>
-                $"{vehicleHash}".ToLower().StartsWith(searchVehicleName) && !vehicleHashes.Contains(vehicleHash)));
-
-            //Name contains
-            vehicleHashes.AddRange(Enum.GetValues(typeof(VehicleHash)).Cast<VehicleHash>().Where(vehicleHash =>
-                $"{vehicleHash}".ToLower().Contains(searchVehicleName) && !vehicleHashes.Contains(vehicleHash)));
-
-            return vehicleHashes;
+            return Enum.GetValues(typeof(VehicleHash)).Cast<VehicleHash>().AsParallel()
+                .Where(vH => searchVehicleName == $"{vH}".ToLower()
+                || $"{vH}".ToLower().StartsWith(searchVehicleName)
+                || $"{vH}".ToLower().Contains(searchVehicleName)).Distinct().ToList();
         }
 
         /// <inheritdoc />
         public List<VehicleHash> GetVehiclesByClass(VehicleClass vehicleClass)
         {
-            return Enum.GetValues(typeof(VehicleHash)).Cast<VehicleHash>()
+            return Enum.GetValues(typeof(VehicleHash)).Cast<VehicleHash>().AsParallel()
                 .Where(vehicleHash => (VehicleClass) API.shared.getVehicleClass(vehicleHash) == vehicleClass).ToList();
         }
 
@@ -43,25 +31,11 @@ namespace EvoMp.Module.VehicleHandler.Server
         public List<VehicleHash> GetVehiclesByIngameName(string searchIngameVehicleName)
         {
             searchIngameVehicleName = searchIngameVehicleName.ToLower();
-            List<VehicleHash> vehicleHashes = new List<VehicleHash>();
 
-            // Name like
-            vehicleHashes.AddRange(Enum.GetValues(typeof(VehicleHash)).Cast<VehicleHash>()
-                .Where(vehicleHash =>
-                    searchIngameVehicleName == API.shared.getVehicleDisplayName(vehicleHash).ToLower() &&
-                    !vehicleHashes.Contains(vehicleHash)));
-
-            //Name starts with
-            vehicleHashes.AddRange(Enum.GetValues(typeof(VehicleHash)).Cast<VehicleHash>().Where(vehicleHash =>
-                API.shared.getVehicleDisplayName(vehicleHash).ToLower().StartsWith(searchIngameVehicleName) &&
-                !vehicleHashes.Contains(vehicleHash)));
-
-            //Name contains
-            vehicleHashes.AddRange(Enum.GetValues(typeof(VehicleHash)).Cast<VehicleHash>().Where(vehicleHash =>
-                API.shared.getVehicleDisplayName(vehicleHash).ToLower().Contains(searchIngameVehicleName) &&
-                !vehicleHashes.Contains(vehicleHash)));
-
-            return vehicleHashes;
+            return Enum.GetValues(typeof(VehicleHash)).Cast<VehicleHash>().AsParallel()
+                .Where(vh => searchIngameVehicleName == API.shared.getVehicleDisplayName(vh)
+                || API.shared.getVehicleDisplayName(vh).ToLower().StartsWith(searchIngameVehicleName)
+                || API.shared.getVehicleDisplayName(vh).ToLower().Contains(searchIngameVehicleName)).Distinct().ToList();
         }
     }
 }
